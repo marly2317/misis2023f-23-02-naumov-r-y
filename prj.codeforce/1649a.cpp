315f72eaa3f759ca93e7dbfca1d02c590017e635
marly2317 <145667431+marly2317@users.noreply.github.com>
2023-10-20 13:30:00 +0300

    Create 1649a.cpp

---
+++ prj.codeforce/1649a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+int main() {
+	int t;
+	std::cin >> t;
+  for (h=0;h<t;h++){
+  	int n;
+  	int k=0;
+  	std::string s;
+  	std::cin >> n;
+  	std::cin >> s;
+  	int sp[1000] = { 0 };
+  	for (int i = 0; i < n; i++) {
+  		std::cout << sp[i];
+  	}
+  	std::string s;
+  	std::cin >> n;
+  	std::cin >> s;
+  	for (int i = 0; i < n; i++) {
+  		if (s[i] == '0'){
+  			sp[i] = i;
+  		}
+  	}
+  	for (int i = 0; i < n; i++) {
+  		if (s[i] == '0') {
+  			k += sp[i] + i;
+  		}
+  	}
+  }
+	std::cout << k;
+
+}
