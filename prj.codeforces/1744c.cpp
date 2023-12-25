43fffdf912ae1aee8f18b151d9053c0b9e230501
marly2317 <marly_matt@mail.ru>
2023-12-26 00:16:23 +0300

    c

--- prj.codeforces/1744c.cpp
+++ prj.codeforces/1744c.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
+
+int t, n;
+char a;
+string s;
+
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m = 0;
-        char c;
-        string s;
-        cin >> n >> c >> s;
-        for (int i = 0; i < s.length(); i++) {
-            if (c == s[i]) m = max(i - m, m);
-        }
-        cout << m << endl;
-    }
-    return 0;
+	cin >> t;
+	while (t--) {
+		cin >> n >> a;
+		cin >> s;
+		s = '*' + s + s;
+		int last = 0;
+		int ans = 0;
+		for (int i = 2 * n; i > 0; --i) {
+			if (s[i] == 'g')last = i;
+			if (s[i] == a)ans = max(ans, last - i);
+		}
+		cout << ans << endl;
+	}
+	return 0;
 }
